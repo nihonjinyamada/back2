0e9379cff40b5320517d1a480a780001adc6a5da
nihonjinyamada <kumamoto.417210@gmail.com>
2025-03-19 17:00:23 +0900

    Initial commit for backend folder

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://front-eta-khaki.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
